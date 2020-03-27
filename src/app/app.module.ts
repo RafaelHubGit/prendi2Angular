561e2964b4b8677dc262ba16c4f349dbd993e0f2
Rafael <rafael.nava.1403@gmail.com>
2020-03-26 22:31:51 -0600

    Se crea Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { VideoMainComponent } from './components/video-main/video-main.component';
+import { ServicesMainComponent } from './components/services-main/services-main.component';
+import { AboutUsMainComponent } from './components/about-us-main/about-us-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    VideoMainComponent,
+    ServicesMainComponent,
+    AboutUsMainComponent
   ],
   imports: [
     BrowserModule,
